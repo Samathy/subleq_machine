293cfeb01827a31d528ebf8214b8f46bb832bd7f
Sam Barratt <samathy@sbarratt.co.uk>
2015-10-12 14:15:36 +0100

    Added TODO

--- subleq_assembler.py
+++ subleq_assembler.py
@@ -1,11 +1,12 @@
-
-
+#!/usr/bin/python3
 asm_values = open("subleq.asm").read().split() 
+print("Generated tokens")
 print (asm_values)
 
 
-with open('subleq.bin', 'wb') as f:
+with open('subleq.bin', 'wb') as f: #TODO Add check for succesful open
     for items in asm_values:
-        print("writing "+str(int(items)))
         f.write(int(items).to_bytes(1,byteorder='big',signed=True))
+
+print("Wrote file")
 f.close()
